a2dd3168e81f8e61fc514fc776f56bbe1834065e
Darkzwer <52218993+Darkzwer@users.noreply.github.com>
2021-05-14 14:56:03 +0300

    some fixes

--- Manga/ViewController.swift
+++ Manga/ViewController.swift
@@ -12,7 +12,7 @@ import Alamofire
 
 class ViewController: UIViewController, UICollectionViewDataSource {
     let networkService = NetworkService()//Ссылка на свифт файл
-    var searchResponse: SearchResponse? = nil//Ссылка на свифт файл
+    let searchResponse: SearchResponse? = nil//Ссылка на свифт файл раньше было var и серч респонс был перемещен из моделей в нетворкинг
     private var timer: Timer?//Объявление таймера
     
     let searchController = UISearchController(searchResultsController: nil)//создание search controller
@@ -27,17 +27,18 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         //setupSearchBar()//вызов SearchBar()
         
         let urlString = "https://api.jikan.moe/v3/manga/1/characters"//my api link
+        //let urlString = "https://api.jikan.moe/v3/top/manga/3"//my api link
         
         //let urlString = "https://itunes.apple.com/search?term=jack+johnson&limit=25"//api link by apple to search data need to add \(searchText) without jack+johnson
         timer?.invalidate()//1:15:27
-        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in self.networkService.request(urlString: urlString) { [weak self] (searchResponse, error) in
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: { (_) in self.networkService.request(urlString: urlString) { [weak self] (searchResponse, error) in
 //            self?.searchResponse = searchResponse//or down string its about printing data
                 searchResponse?.characters.map({(track) in//MangaResponce?.top.map({ (title) in
                     self?.collectionView.reloadData()
-                    print(track.name)//print(manga.title)//вывод изображений
-                    //print(track.artworkUrl60)//вывод изображений
-                    //print(track.artistName)
-                    //print(track.collectionName)
+                    print(track.name)//print(manga.name)//вывод имени в консоль
+                    print(track.image_url)//вывод изображений в консоль
+                    print(track.role)//вывод роли в консоль
+                    print(track.mal_id)//ид изображений в консоль
                 })
             }
         })
@@ -50,9 +51,9 @@ class ViewController: UIViewController, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCollectionViewCell
         let track = searchResponse?.characters[indexPath.row]//1:15:27
-        //print("track?.artworkUrl60:", track?.artworkUrl60)//вывод изображений при запросе
-        //print("track?.collectionName:", track?.collectionName)//вывод названий при запросе
-        //cell.nameLabel?.text = track?.title//nameLabl из аутлета текста в CustomCollectionViewCell
+        print("track?.image_url:", track?.image_url)//вывод изображений при запросе
+        print("track?.role:", track?.role)//вывод названий при запросе
+        cell.nameLabel?.text = track?.name//nameLabel из аутлета текста в CustomCollectionViewCell
         return cell
         }
     }
